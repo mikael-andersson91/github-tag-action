a2cc5b12eb5a8b2e841accb8e34ae6a742a2955d
Mikael Andersson <mikael.andersson3@ingka.ikea.com>
2022-11-09 17:27:53 +0100

    fix: logic for determining prerelease
    Logic to determine prerelease based on if the triggering event was a pull request or if run on prerelease branch.

--- src/action.ts
+++ src/action.ts
@@ -64,7 +64,7 @@ export default async function main() {
     .split(',')
     .some((branch) => currentBranch.match(branch));
   const isPullRequest = isPr(GITHUB_EVENT_NAME);
-  const isPrerelease = !isReleaseBranch && !isPullRequest && isPreReleaseBranch;
+  const isPrerelease = !isReleaseBranch && (isPullRequest || isPreReleaseBranch);
 
   // Sanitize identifier according to
   // https://semver.org/#backusnaur-form-grammar-for-valid-semver-versions
